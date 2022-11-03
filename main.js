5e231e6e89447486d1fcc8f096d7d47b5590fed9
DonnieAndMooie <owenjeaves@gmail.com>
2022-11-03 11:16:23 +0000

    Create a function called operate
    Operate takes 2 numbers and an operator and calls the correct function

--- main.js
+++ main.js
@@ -1,24 +1,42 @@
 function add(num1, num2){
-    let output = num1 + num2
-    console.log(output)
-    return (output)
+    let output = num1 + num2;
+    console.log(output);
+    return (output);
 }
 
 function subtract(num1, num2){
-    let output = num1 - num2
-    console.log(output)
-    return (output)
+    let output = num1 - num2;
+    console.log(output);
+    return (output);
 }
 
 function multiply(num1, num2){
-    let output = num1 * num2
-    console.log(output)
-    return (output)
+    let output = num1 * num2;
+    console.log(output);
+    return (output);
 }
 
 function divide(num1, num2){
-    let output = num1 / num2
-    console.log(output)
-    return (output)
+    let output = num1 / num2;
+    console.log(output);
+    return (output);
+}
+
+function operate(num1, num2, operator){
+    if (operator === "+"){
+        add(num1, num2);
+    }
+    else if (operator === "-"){
+        subtract(num1, num2);
+    }
+    else if (operator === "*"){
+        multiply(num1, num2);
+    }
+    else if (operator === "/"){
+        divide(num1, num2);
+    }
+    else{
+        console.log("Something went wrong")
+    }
 }
 
