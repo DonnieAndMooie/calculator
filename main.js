27ac95e82a388f1956ea5cd8ae3a7649b05f4cec
DonnieAndMooie <owenjeaves@gmail.com>
2022-11-03 14:09:03 +0000

    Calculate equations and display answer on screen

--- main.js
+++ main.js
@@ -24,71 +24,148 @@ function divide(num1, num2){
 
 function operate(num1, num2, operator){
     if (operator === "+"){
-        add(num1, num2);
+        return add(num1, num2);
     }
     else if (operator === "-"){
-        subtract(num1, num2);
+        return subtract(num1, num2);
     }
     else if (operator === "*"){
-        multiply(num1, num2);
+        return multiply(num1, num2);
     }
     else if (operator === "/"){
-        divide(num1, num2);
+        return divide(num1, num2);
     }
     else{
-        console.log("Something went wrong")
+        console.log("Something went wrong");
     }
 }
 
-screen = document.querySelector(".screen")
-btn1 = document.querySelector(".btn1")
-btn2 = document.querySelector(".btn2")
-btn3 = document.querySelector(".btn3")
-btn4 = document.querySelector(".btn4")
-btn5 = document.querySelector(".btn5")
-btn6 = document.querySelector(".btn6")
-btn7 = document.querySelector(".btn7")
-btn8 = document.querySelector(".btn8")
-btn9 = document.querySelector(".btn9")
-btn0 = document.querySelector(".btn0")
+screen = document.querySelector(".screen");
+btn1 = document.querySelector(".btn1");
+btn2 = document.querySelector(".btn2");
+btn3 = document.querySelector(".btn3");
+btn4 = document.querySelector(".btn4");
+btn5 = document.querySelector(".btn5");
+btn6 = document.querySelector(".btn6");
+btn7 = document.querySelector(".btn7");
+btn8 = document.querySelector(".btn8");
+btn9 = document.querySelector(".btn9");
+btn0 = document.querySelector(".btn0");
+btnMultiply = document.querySelector(".btnMultiply");
+btnDivide = document.querySelector(".btnDivide");
+btnAdd = document.querySelector(".btnAdd");
+btnSubtract = document.querySelector(".btnSubtract");
+btnEquals = document.querySelector(".btnEquals");
 
 let displayValue = ""
 
 btn1.addEventListener("click", ()=>{
-    displayValue += "1"
+    displayValue += "1";
     updateScreen()})
 btn2.addEventListener("click", ()=>{
-    displayValue += "2"
+    displayValue += "2";
     updateScreen()})
 btn3.addEventListener("click", ()=>{
-    displayValue += "3"
+    displayValue += "3";
     updateScreen()})
 btn4.addEventListener("click", ()=>{
-    displayValue += "4"
+    displayValue += "4";
     updateScreen()})
 btn5.addEventListener("click", ()=>{
-    displayValue += "5"
+    displayValue += "5";
     updateScreen()})
 btn6.addEventListener("click", ()=>{
-    displayValue += "6"
+    displayValue += "6";
     updateScreen()})
 btn7.addEventListener("click", ()=>{
-    displayValue += "7"
+    displayValue += "7";
     updateScreen()})
 btn8.addEventListener("click", ()=>{
-    displayValue += "8"
+    displayValue += "8";
     updateScreen()})
 btn9.addEventListener("click", ()=>{
-    displayValue += "9"
+    displayValue += "9";
     updateScreen()})
 btn0.addEventListener("click", ()=>{
-    displayValue += "0"
+    displayValue += "0";
     updateScreen()})
 
+btnAdd.addEventListener("click", ()=>{
+    displayValue += "+";
+    disableOperators()
+    updateScreen()})
+btnSubtract.addEventListener("click", ()=>{
+    displayValue += "-";
+    disableOperators()
+    updateScreen()})
+btnMultiply.addEventListener("click", ()=>{
+    displayValue += "×";
+    disableOperators()
+    updateScreen()})
+btnDivide.addEventListener("click", ()=>{
+    displayValue += "÷"
+    disableOperators()
+    updateScreen()})
+
+btnEquals.addEventListener("click", () => {
+    if (displayValue.includes("+")){
+        const array = displayValue.split("+");
+        let num1 = Number(array[0]);
+        let num2 = Number(array[1]);
+        displayValue = operate(num1, num2, "+");
+        displayValue = displayValue.toString();
+        updateScreen();
+        enableOperators()
+    }
+    else if (displayValue.includes("-")){
+        const array = displayValue.split("-");
+        let num1 = Number(array[0]);
+        let num2 = Number(array[1]);
+        displayValue = operate(num1, num2, "-");
+        displayValue = displayValue.toString();
+        updateScreen();
+        enableOperators()
+    }
+    else if (displayValue.includes("×")){
+        const array = displayValue.split("×");
+        let num1 = Number(array[0]);
+        let num2 = Number(array[1]);
+        displayValue = operate(num1, num2, "*");
+        displayValue = displayValue.toString();
+        updateScreen();
+        enableOperators()
+    }
+    else if (displayValue.includes("÷")){
+        const array = displayValue.split("÷");
+        let num1 = Number(array[0]);
+        let num2 = Number(array[1]);
+        displayValue = operate(num1, num2, "/");
+        displayValue = displayValue.toString();
+        updateScreen();
+        enableOperators()
+    }
+})
+
 function updateScreen(){
-screen.textContent = displayValue
+    if (displayValue.length <= 9){
+        screen.textContent = displayValue 
+    }
+
 }
 
+function disableOperators(){
+    btnAdd.disabled  = true
+    btnSubtract.disabled  = true
+    btnMultiply.disabled  = true
+    btnDivide.disabled  = true
+}
+
+function enableOperators(){
+    btnAdd.disabled  = false
+    btnSubtract.disabled  = false
+    btnMultiply.disabled  = false
+    btnDivide.disabled  = false
+}
 
 
 
