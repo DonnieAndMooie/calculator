1c84cfea80db0597bb3b47e9dd33fd5bfe653f4c
DonnieAndMooie <owenjeaves@gmail.com>
2022-11-03 12:07:59 +0000

    Add event listeners to number buttons so display updates

--- main.js
+++ main.js
@@ -40,3 +40,55 @@ function operate(num1, num2, operator){
     }
 }
 
+screen = document.querySelector(".screen")
+btn1 = document.querySelector(".btn1")
+btn2 = document.querySelector(".btn2")
+btn3 = document.querySelector(".btn3")
+btn4 = document.querySelector(".btn4")
+btn5 = document.querySelector(".btn5")
+btn6 = document.querySelector(".btn6")
+btn7 = document.querySelector(".btn7")
+btn8 = document.querySelector(".btn8")
+btn9 = document.querySelector(".btn9")
+btn0 = document.querySelector(".btn0")
+
+let displayValue = ""
+
+btn1.addEventListener("click", ()=>{
+    displayValue += "1"
+    updateScreen()})
+btn2.addEventListener("click", ()=>{
+    displayValue += "2"
+    updateScreen()})
+btn3.addEventListener("click", ()=>{
+    displayValue += "3"
+    updateScreen()})
+btn4.addEventListener("click", ()=>{
+    displayValue += "4"
+    updateScreen()})
+btn5.addEventListener("click", ()=>{
+    displayValue += "5"
+    updateScreen()})
+btn6.addEventListener("click", ()=>{
+    displayValue += "6"
+    updateScreen()})
+btn7.addEventListener("click", ()=>{
+    displayValue += "7"
+    updateScreen()})
+btn8.addEventListener("click", ()=>{
+    displayValue += "8"
+    updateScreen()})
+btn9.addEventListener("click", ()=>{
+    displayValue += "9"
+    updateScreen()})
+btn0.addEventListener("click", ()=>{
+    displayValue += "0"
+    updateScreen()})
+
+function updateScreen(){
+screen.textContent = displayValue
+}
+
+
+
+
